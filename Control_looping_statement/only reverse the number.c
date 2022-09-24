6b6a2c7422169a51a3a54fd8ee945940e6b69a60
ankitabagal808 <103379119+ankitabagal808@users.noreply.github.com>
2022-09-24 20:42:08 +0530

    Add files via upload

---
+++ Control_looping_statement/only reverse the number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int  n ;
+
+    printf("Enter value : ");
+    scanf("%d",&n);
+    
+    while(n>=1)
+    {
+       printf("%d\n",n);
+       n--;
+    }
+    return 0;
+}
