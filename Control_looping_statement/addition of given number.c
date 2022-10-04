ef8dc0b33a048cf98492e47f22cab7b3d2d44269
ankitabagal808 <103379119+ankitabagal808@users.noreply.github.com>
2022-10-04 23:03:42 +0530

    Add files via upload

---
+++ Control_looping_statement/addition of given number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   int no=0, sum=0, cnt=0, num=0;
+   printf("Enter number which have you :");
+   scanf("%d",&num);
+   for(cnt=1;cnt<=num;cnt++)
+   {
+       printf("\n\t Enter number %d =",cnt);
+       scanf("%d",&no);
+       sum=sum+no;
+   }
+   printf("\n summation of given numbers =%d",sum);
+   return 0;
+}
