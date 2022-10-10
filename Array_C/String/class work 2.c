cb24b05c41523635c488d72271ac48b63326dfea
ankitabagal808 <103379119+ankitabagal808@users.noreply.github.com>
2022-10-10 20:29:44 +0530

    Add files via upload

---
+++ Array_C/String/class work 2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    char cSrc[50]={'\0'};
+    int i=0;
+    
+    puts("\n Enter a string :");
+    gets(cSrc);
+    
+    while(cSrc[i]!='\0')
+    {
+      if(cSrc[i]>='a'  && cSrc[i]<='z')
+       {
+          cSrc[i]=cSrc[i]-32;
+       }
+       i++;
+    }
+    
+    printf("\n string is= %s",cSrc); 
+   return 0;
+}
+
