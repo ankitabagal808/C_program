c0ff1623d6796ee16b10ddab55027db7ad5c3214
ankitabagal808 <103379119+ankitabagal808@users.noreply.github.com>
2022-10-10 20:46:40 +0530

    Add files via upload

---
+++ Array_C/String/class work 3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    char cSrc[50]={'\0'};
+    int i=0;
+    
+    puts("\n Enter a string :");
+    gets(cSrc);
+    
+    while(cSrc[i]!='\0')
+    {
+       if(cSrc[i]>='A'  && cSrc[i]<='Z')
+       {
+          cSrc[i]=cSrc[i]+32;
+       }
+       i++;
+    }
+    
+    printf("\n Small string is= %s",cSrc); 
+   return 0;
+}
