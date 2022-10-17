8da650fe36740a56bbe37cde9c0ea16254ff4339
ankitabagal808 <103379119+ankitabagal808@users.noreply.github.com>
2022-10-17 23:28:52 +0530

    Add files via upload

---
+++ Basic_C_program/Program02-Display information.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("\n ************Student Information Database************");
+    
+    printf("\n\n********************************************************");
+    
+    printf("\n\n%4d \t%-7s %3d %3d %3d %0.2f",255,"Amit",9,81,150,50.00);
+    printf("\n%4d \t%-7s %3d %3d %3d %0.2f",02,"Sujata",70,80,200,66.667) ;
+    printf("\n%4d \t%-7s %3d %3d %3d %0.2f",150,"Ajay",82,18,170,56.6677);
+    
+    printf("\n\n********************************************************");
+    getch();
+    return 0;
+}
