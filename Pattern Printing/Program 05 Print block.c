cdd93e1241523078b5ba60a6bd3b44f0a18eb11b
ankitabagal808 <103379119+ankitabagal808@users.noreply.github.com>
2022-10-19 01:18:39 +0530

    Add files via upload

---
+++ Pattern Printing/Program 05 Print block.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+     int r=0, c=0,k=0;
+     
+     printf ("Enter number :");
+     scanf ("%d",&k);
+     
+     for (r=1;r<=k;r++)
+     {
+      for (c=1;c<=k;c++)
+       {
+          printf(" * ");
+       }
+      printf ("\n");
+     }
+     
+     getch();
+     return 0;
+}
