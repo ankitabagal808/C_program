eb6823485a63afc1b897938e2b30e433feed26ad
ankitabagal808 <103379119+ankitabagal808@users.noreply.github.com>
2022-10-19 00:56:05 +0530

    Add files via upload

---
+++ Pattern Printing/Program 01 Print N.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+     int r=0, c=0,k=0;
+    
+     printf ("Enter number :");
+     scanf ("%d",&k);
+     
+     for (r=1;r<=k;r++)
+     {
+       for (c=1;c<=k;c++)
+       {
+          if(c==1|| c==r|| k==c)
+          {
+            printf(" * ");
+          }
+          else
+          {
+            printf ("   ");
+          }
+        }
+        printf ("\n");
+     }
+    
+     getch();
+     return 0;
+}
