4ef96bef2b811a872f95de6c830fa9ffa783809c
ankitabagal808 <103379119+ankitabagal808@users.noreply.github.com>
2022-10-19 17:12:02 +0530

    Add files via upload

---
+++ looping statement/Program 05 Count digits in given numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+     int no=0, dcnt=0, temp=0;
+     
+     up:
+     printf("\n Enter positive  number to calculate its digit sum=");
+     scanf("%d",&no);
+  
+     temp=no;
+     while(temp>0)
+     {
+       dcnt++;
+       temp=temp/10;
+     }
+     printf("\n\n Number of digits in given number %d is =%d",no,dcnt);
+  
+    getch();
+    return 0;
+}
