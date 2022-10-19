112ff930e67093b6e3a0c2756e5e9aa8422925ce
ankitabagal808 <103379119+ankitabagal808@users.noreply.github.com>
2022-10-19 09:41:22 +0530

    Add files via upload

---
+++ if_else Statement/Program 06 Maximum number between two numbers.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+int main()
+{
+     int num1=0,num2=0;
+     
+     printf("\n Enter first number :");
+     scanf("%d",&num1);
+     printf("\n Enter second number :");
+     scanf("%d",&num2);
+    
+     if(num1==num2)
+     {
+         printf("\n Both numbers are equal.");
+         goto Down;
+     }
+     
+     (num1>num2)? printf("\n Number %d is maximum.",num1): printf("\n Number %d is maximum",num2);
+     
+     Down:
+     printf("\n\t\tTHANK YOU");
+     
+     getch();
+     return 0;
+}
+
+
+
+
+
