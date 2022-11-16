75527338b0c33572f52322426e23d3fef56abed9
ankitabagal808 <103379119+ankitabagal808@users.noreply.github.com>
2022-11-16 22:34:11 +0530

    Add files via upload

---
+++ C Assignment - Array/Program 11 Pattern printing.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+     int n=5, c=0, r=0;
+     
+     for(r=1;r<=n;r++)
+     {
+        for(c=1;c<=r;c++) 
+        {
+           printf("Q");
+        } 
+        printf("\n");
+     }
+     
+     getch();
+     return 0;
+}
